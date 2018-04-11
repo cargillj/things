05f07a24c5e78cc03d5d9edb2f438d8d9682b0ff
johnCargill <jhc@callbox.com>
2018-04-11 17:44:54 -0500

    Initial livereload code

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
-// const livereload = require('gulp-livereload')
+const livereload = require('gulp-livereload')
 const nodemon = require('gulp-nodemon')
 
 const tsProject = ts.createProject('tsconfig.json')
+
+gulp.task('markup', () => {
+    gulp.src('src/app/**.html')
+        .pipe(gulp.dest('dist/app'))
+        .pipe(livereload())
+})
 
 gulp.task('scripts', () => {
     return tsProject.src()
@@ -11,15 +17,20 @@ gulp.task('scripts', () => {
         .js.pipe(gulp.dest('dist'));
 })
 
-gulp.task('build', ['scripts'])
+gulp.task('build', ['markup','scripts'])
 
 gulp.task('nodemon', () => {
     nodemon({
         script: './dist/server/server.js',
-        watch: 'src',
+        watch: 'src/server',
         ext: 'ts',
         tasks: ['build']
     })
+})
+
+gulp.task('watch', () => {
+    livereload.listen()
+    gulp.watch('src/app/**.html', ['markup'])
 })
 
 gulp.task('default', ['build', 'nodemon'])
