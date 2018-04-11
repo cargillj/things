c6da6a85edab95573e99b28c58f8c2d88af5820f
johnCargill <jhc@callbox.com>
2018-04-10 22:12:53 -0500

    initial livereload code

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
+const server = require('gulp-express')
+
 const tsProject = ts.createProject('tsconfig.json')
 
-gulp.task('default', function () {
+gulp.task('scripts', () => {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest('dist'));
-});
+})
+
+gulp.task('dev-server', () => {
+    process.env.node_ENV = 'development'
+    server.run(['./dist/server/server.js'])
+    // gulp.watch('src/**/*.ts', ['scripts', server.run])
+    gulp.watch('src/**/*.ts', (event) => {
+        gulp.start('scripts')
+        server.run()
+        server.notify(event)
+    })
+})
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as liveReload from 'connect-livereload'
 import {api} from './routes/api/api'
 
-const app = express()
+export const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
+app.use(liveReload())
 
 const port = process.env.PORT || 8080
 
